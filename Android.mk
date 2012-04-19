20c80742edecb61e4812a7bda48fa719ab859c9c
Rob Beane <robbeane@gmail.com>
2012-04-19 12:02:19 -0400

    Revert "external/skia: Add QC hooks"
    This reverts commit 06d6d13f516a1b39a3f7f022ef7f05c73716ab91.

--- Android.mk
+++ Android.mk
@@ -263,11 +263,6 @@ LOCAL_STATIC_LIBRARIES := \
 	libwebp-decode \
 	libwebp-encode
 
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-	LOCAL_WHOLE_STATIC_LIBRARIES += libqc-skia
-endif
-
-
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/src/core \
 	$(LOCAL_PATH)/include/core \
@@ -348,10 +343,6 @@ LOCAL_SRC_FILES:= \
   src/gpu/SkGrTexturePixelRef.cpp \
   src/gpu/SkGrFontScaler.cpp \
 	src/gpu/GrPrintf_skia.cpp
-
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-	LOCAL_WHOLE_STATIC_LIBRARIES += libqc-skia
-endif
 
 LOCAL_SHARED_LIBRARIES := \
   libcutils \
